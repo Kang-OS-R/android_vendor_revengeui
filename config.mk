655e03065ef52882b1d209f201987c0738921c67
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-03-05 21:17:16 +0000

    Kangui: update Bootanimation and wallpaper

--- config.mk
+++ config.mk
@@ -38,13 +38,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay/common/
 
 # Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation_720.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation_1080.zip:system/media/bootanimation.zip
-else
-     PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation_1080.zip:system/media/bootanimation.zip
-endif
+PRODUCT_COPY_FILES += vendor/revengeui/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Apps
 PRODUCT_PACKAGES += \
